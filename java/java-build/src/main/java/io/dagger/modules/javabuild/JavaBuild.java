c15c92f765a4c4ecc3430a19400e38e163af5776
robertonav20 <roberto.nav20@gmail.com>
2025-04-25 00:58:41 +0000

    initial commit

--- java/java-build/src/main/java/io/dagger/modules/javabuild/JavaBuild.java
+++ java/java-build/src/main/java/io/dagger/modules/javabuild/JavaBuild.java
@@ -20,33 +20,53 @@ public class JavaBuild {
 
   /** Publish the application container after building and testing it on-the-fly */
   @Function
-  public String publish(@DefaultPath("/") Directory source, String repository, Optional<String> username, Optional<Secret> password, String appName, String version)
+  public String publish(
+      @DefaultPath("/") Directory source,
+      String repository,
+      Optional<String> username,
+      Optional<Secret> password,
+      String appName,
+      String version)
       throws InterruptedException, ExecutionException, DaggerQueryException {
-    this.test(source);
+    test(source);
+    Directory target = buildArtifact(source);
+
     if (password.isPresent()) {
-      return this.build(source)
+      deploy(target, repository, username.get(), password.get(), appName, version);
+      return buildImage(target)
         .withRegistryAuth(repository, username.get(), password.get())
         .publish(String.format("%s/%s:%s", repository, appName, version));
     }
 
-    return this.build(source).publish(String.format("%s/%s:%s", repository, appName, version));
+    return buildImage(target)
+      .publish(String.format("%s/%s:%s", repository, appName, version));
   }
 
   /** Build the application container */
   @Function
-  public Container build(@DefaultPath("/") Directory source)
+  public Directory buildArtifact(@DefaultPath("/") Directory source)
       throws InterruptedException, ExecutionException, DaggerQueryException {
-    Directory build = this.buildEnv(source)
-        .withExec(List.of("mvn", "clean", "package", "-DskipTests")).directory("./target");
-    return dag().container().from("eclipse-temurin:17-jre-alpine").withDirectory("/app", build)
-        .withExposedPort(8080).withEntrypoint(List.of("java", "-jar", "/app/app.jar"));
+    return buildEnv(source)
+        .withExec(List.of("mvn", "clean", "package", "-DskipTests"))
+        .directory("./target");
+  }
+
+  /** Build the application container */
+  @Function
+  public Container buildImage(@DefaultPath("/") Directory build)
+      throws InterruptedException, ExecutionException, DaggerQueryException {
+    return dag().container()
+        .from("eclipse-temurin:17-jre-alpine")
+        .withDirectory("/app", build)
+        .withExposedPort(8080)
+        .withEntrypoint(List.of("java", "-jar", "/app/app.jar"));
   }
 
   /** Return the result of running unit tests */
   @Function
   public String test(@DefaultPath("/") Directory source)
       throws InterruptedException, ExecutionException, DaggerQueryException {
-    return this.buildEnv(source).withExec(List.of("mvn", "test")).stdout();
+    return buildEnv(source).withExec(List.of("mvn", "test")).stdout();
   }
 
   /** Build a ready-to-use development environment */
@@ -54,7 +74,35 @@ public class JavaBuild {
   public Container buildEnv(@DefaultPath("/") Directory source)
       throws InterruptedException, ExecutionException, DaggerQueryException {
     CacheVolume m2Cache = dag().cacheVolume("m2");
-    return dag().container().from("maven:3.9-eclipse-temurin-17-alpine")
-        .withDirectory("/src", source).withMountedCache("/root/.m2", m2Cache).withWorkdir("/src");
+    return dag().container()
+        .from("maven:3.9-eclipse-temurin-17-alpine")
+        .withDirectory("/src", source)
+        .withMountedCache("/root/.m2", m2Cache)
+        .withWorkdir("/src");
+  }
+
+  /** Deploy the application artifact to Nexus repository */
+  @Function
+  public Container deploy(
+      @DefaultPath("/") Directory source,
+      String url,
+      String username,
+      Secret password,
+      String artifactId,
+      String version)
+      throws InterruptedException, ExecutionException, DaggerQueryException {
+    // Assuming the built artifact is located in the target directory
+    return buildEnv(source)
+        .withEnvVariable("NEXUS_SERVER_USERNAME", username)
+        .withEnvVariable("NEXUS_SERVER_PASSWORD", password.plaintext())
+        .withExec(List.of("mvn", "deploy:deploy-file",
+            "-DgroupId=com.example",
+            "-DartifactId=" + artifactId,
+            "-Dversion=" + version,
+            "-Dpackaging=jar",
+            "-Dfile=app.jar",
+            "-DrepositoryId=maven-releases",
+            "-Durl=" + url,
+            "-DserverId=nexus-server"));
   }
 }
