f81a696a4c7b99e0394a7bf6c03c10396e66ac86
robertonav20 <roberto.nav20@gmail.com>
2025-07-03 20:30:33 +0000

    Add spotless check to hello world

--- hello-world/src/main/java/com/example/app/HelloWorldApplication.java
+++ hello-world/src/main/java/com/example/app/HelloWorldApplication.java
@@ -1,5 +1,8 @@
 package com.example.app;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.function.Function;
 import java.util.stream.Collectors;
@@ -13,54 +16,47 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletRequest;
 
 @RestController
 @SpringBootApplication
 public class HelloWorldApplication {
 
-	@Tag(name = "HelloWorldController")
-	@RestController
-	public static class HelloWorldController {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
 
-		@RequestMapping("/hello")
-		public String home() {
-			return "Hello World!";
-		}
+    @Bean
+    public HttpExchangeRepository httpTraceRepository() {
+        return new InMemoryHttpExchangeRepository();
+    }
 
-	}
+    @Tag(name = "EchoController")
+    @RestController
+    public static class EchoController {
 
-	@Tag(name = "EchoController")
-	@RestController
-	public static class EchoController {
+        public static record EchoResponse(Object protocol, Object method, Object path, Object headers,
+                Object queryString, Object body) {
+        }
 
-		@RequestMapping(value = "/echo", consumes = MediaType.APPLICATION_JSON_VALUE)
-		public ResponseEntity<EchoResponse> echo(HttpServletRequest request, @RequestBody Object object)
-				throws JsonProcessingException {
-			return ResponseEntity.ok()
-					.body(new EchoResponse(request.getProtocol(), request.getMethod(),
-							request.getRequestURI(),
-							Collections.list(request.getHeaderNames()).stream()
-									.collect(Collectors.toMap(Function.identity(),
-											h -> Collections.list(request.getHeaders(h)))),
-							request.getQueryString(), object));
-		}
+        @RequestMapping(value = "/echo", consumes = MediaType.APPLICATION_JSON_VALUE)
+        public ResponseEntity<EchoResponse> echo(HttpServletRequest request, @RequestBody Object object)
+                throws JsonProcessingException {
+            return ResponseEntity.ok()
+                    .body(new EchoResponse(request.getProtocol(), request.getMethod(), request.getRequestURI(),
+                            Collections.list(request.getHeaderNames()).stream()
+                                    .collect(Collectors.toMap(Function.identity(),
+                                            h -> Collections.list(request.getHeaders(h)))),
+                            request.getQueryString(), object));
+        }
+    }
 
-		public static record EchoResponse(Object protocol, Object method, Object path,
-				Object headers, Object queryString, Object body) {
-		}
+    @Tag(name = "HelloWorldController")
+    @RestController
+    public static class HelloWorldController {
 
-	}
-
-	@Bean
-	public HttpExchangeRepository httpTraceRepository() {
-		return new InMemoryHttpExchangeRepository();
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldApplication.class, args);
-	}
-
+        @RequestMapping("/hello")
+        public String home() {
+            return "Hello World!";
+        }
+    }
 }
